dc8250e0815e05f4c6221da74727621da081b0ca
skiftare <74261401+Skiftare@users.noreply.github.com>
2023-10-06 08:52:22 +0500

    Update fuckingsearch.bat

--- OSi/fuckingsearch.bat
+++ OSi/fuckingsearch.bat
@@ -1,22 +1,11 @@
 @echo off
-
 setlocal
-
-rem change to the correct directory
 cd /d C:\
 echo search in C disk
-rem count the files
 dir /b %1 /s 2> nul | find "" /v /c > %temp%\count
 set /p _count=<%temp%\count
-
-rem cleanup
 del %temp%\count
-
-rem output the number of files
 echo Files found : %_count%
-
-rem list the files
 echo Files Paths :
 dir /b %1 /s
-
 endlocal
